ed10424cdf2b2a313aafd16f22465b1888de02a7
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-04-27 01:30:10 +0900

    [feat] 스코어 UI 연동 추가
    GameManager의 score를 UI text에 연동

---
+++ Medival_DodgeGame/Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI tmp;
+    GameManager gm;
+
+    private void Start()
+    {
+        gm = GameManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gm.GmState == GameState.OnGame)
+        {
+            float time = gm.scoreTime;
+            int seconds = Mathf.FloorToInt(time);           // 초
+            int milliseconds = Mathf.FloorToInt((time * 100) % 100);  // 밀리초 (1/100초 단위)
+            string score = string.Format("{0:00}:{1:00}", seconds, milliseconds);
+
+            tmp.text = score;
+        }
+    }
+}
